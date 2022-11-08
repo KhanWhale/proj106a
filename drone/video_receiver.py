a2c159b66fc229587892d638ed159a3b6484547d
Aniruddh Khanwale <akhanwale@berkeley.edu>
2022-11-08 09:56:22 -0800

    Add rudimentary drone controllers

---
+++ drone/video_receiver.py
@@ -0,0 +1,43 @@
+import socket
+
+localIP     = "0.0.0.0"
+
+videoPort   = 11111
+
+bufferSize  = 1024
+ 
+
+# Create a datagram socket
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+ 
+
+# Bind to address and ip
+
+UDPServerSocket.bind((localIP, videoPort))
+
+ 
+
+print("UDP server up and listening")
+
+ 
+
+# Listen for incoming datagrams
+
+while(True):
+
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
+    message = bytesAddressPair[0]
+
+    address = bytesAddressPair[1]
+
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP  = "Client IP Address:{}".format(address)
+    
+    print(clientMsg)
+    print(clientIP)
+
+   
+
